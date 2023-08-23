2a22380ad49263ac2f80a0d172565d637d6f4179
Manuel Gualito <im.manu@comunidad.unam.mx>
2023-08-23 12:59:20 -0600

    Se completo Create, Update y Delete.

--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Task
 
 # Create your views here.
@@ -12,3 +14,22 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
+
+# esta vista es para Crear una tarea
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks') #esto sirve para redireccionar al usuario
+
+# esta vista es para Actualizar una task
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+# esta vista es para Eliminar una task
+class DeleteView(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
