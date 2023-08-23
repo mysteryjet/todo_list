a2c4eff25f37a0965aac17a1b5e90a4e3ef89185
Manuel Gualito <im.manu@comunidad.unam.mx>
2023-08-23 14:17:41 -0600

    Se completó restringir páginas a usuarios sin login

--- base/views.py
+++ base/views.py
@@ -6,6 +6,7 @@ from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView
 
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Task
 
 # vista para login
@@ -17,30 +18,30 @@ class CustomLoginView(LoginView):
     def get_success_url(self):
         return reverse_lazy('tasks')
 # Create your views here.
-class TaskList(ListView):# regresa un template con un queryset de datos
+class TaskList(LoginRequiredMixin, ListView):# regresa un template con un queryset de datos
     model = Task
     context_object_name = 'tasks'
 
-class TaskDetail(DetailView):
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
 
 # esta vista es para Crear una tarea
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks') #esto sirve para redireccionar al usuario
 
 # esta vista es para Actualizar una task
-class TaskUpdate(UpdateView):
+class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
 
 
 # esta vista es para Eliminar una task
-class DeleteView(DeleteView):
+class DeleteView(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('tasks')
