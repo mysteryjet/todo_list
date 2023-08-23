0fec2149cf86ad2505e36323620de2402e1ee2a9
Manuel Gualito <im.manu@comunidad.unam.mx>
2023-08-23 14:10:32 -0600

    Se completó login y logout

--- base/views.py
+++ base/views.py
@@ -3,8 +3,19 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView
+
 from .models import Task
 
+# vista para login
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
 # Create your views here.
 class TaskList(ListView):# regresa un template con un queryset de datos
     model = Task
