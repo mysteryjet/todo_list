a1ff7ea9fea6c8493d1871e619dd0932bcbffe24
Manuel Gualito <im.manu@comunidad.unam.mx>
2023-08-23 15:23:21 -0600

    Se completó Registro de usuarios y restriccion de vistas si no está registrado y si está logueado

--- base/views.py
+++ base/views.py
@@ -1,13 +1,15 @@
 from typing import Any, Dict
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView
-
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
 from .models import Task
 
 # vista para login
@@ -18,7 +20,27 @@ class CustomLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('tasks')
-# Create your views here.
+
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        user = form.save()
+        #cuando esto sea válido, se hará login al usuario
+        if user is not None: # esto indica si el usuario fue creado correctamente
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+    
+    def get (self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args, **kwargs)
+
+
+# muestra la vista de las task que se tienen
 class TaskList(LoginRequiredMixin, ListView):# regresa un template con un queryset de datos
     model = Task
     context_object_name = 'tasks'
@@ -38,7 +60,7 @@ class TaskDetail(LoginRequiredMixin, DetailView):
 # esta vista es para Crear una tarea
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = {'title', 'description', 'complete'}
+    fields = {'title', 'complete', 'description'}
     success_url = reverse_lazy('tasks') #esto sirve para redireccionar al usuario
 
     def form_valid(self, form):
@@ -47,7 +69,7 @@ class TaskCreate(LoginRequiredMixin, CreateView):
 # esta vista es para Actualizar una task
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = {'title', 'description', 'complete'}
+    fields = {'complete', 'description', 'title'}
     success_url = reverse_lazy('tasks')
 
 
