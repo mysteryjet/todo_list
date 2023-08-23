b1a8c72a9c78db45afaca9fe0c4d8ef33a5a659a
Manuel Gualito <im.manu@comunidad.unam.mx>
2023-08-23 15:38:22 -0600

    Se completó barra de Búsqueda

--- base/views.py
+++ base/views.py
@@ -50,6 +50,13 @@ class TaskList(LoginRequiredMixin, ListView):# regresa un template con un querys
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
         context['count'] = context['tasks'].filter(complete=False).count()
+        
+        # para buscar texto dentro de las tasks
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(title__icontains=search_input)
+
+        context['search_input'] = search_input
         return context
 
 class TaskDetail(LoginRequiredMixin, DetailView):
